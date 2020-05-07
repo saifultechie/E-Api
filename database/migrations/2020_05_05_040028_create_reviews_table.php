69495d75d27abb0b6388465e9b5f6d0da8caa479
saifultechie <saifultechie@gmail.com>
2020-05-07 14:03:08 +0600

    migration created

--- database/migrations/2020_05_05_040028_create_reviews_table.php
+++ database/migrations/2020_05_05_040028_create_reviews_table.php
@@ -15,7 +15,15 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('product_id')->unsigned();
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+
         });
     }
 
